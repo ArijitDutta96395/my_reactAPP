6cc4fa3fdfb511afe8b093cc07c55456594d478b
ArijitDutta96395 <joyajitdutta2014@gmail.com>
2024-11-08 14:31:10 +0530

    my app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [showText, setShowText] = useState(false);
+
+  const handleClick = () => {
+    setShowText(prevShowText => !prevShowText);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="my-button" onClick={handleClick}>
+        {showText ? 'Hide Text' : 'Show Text'}
+      </button>
+      {showText && <p className="my-text">Hayyy girls, Arijit This Side...</p>}
     </div>
   );
-}
+};
 
 export default App;
